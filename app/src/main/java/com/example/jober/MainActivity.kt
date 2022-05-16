b1a9bf6d42f66381c7d1be8eec888943bdee539a
Ale <ale.grassi03@gmail.com>
2022-05-16 19:04:06 +0200

    Aggiunta barra di navigazione e creti fragments iniziali

--- app/src/main/java/com/example/jober/MainActivity.kt
+++ app/src/main/java/com/example/jober/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.jober
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+
+        val offersFragment=OffersFragment()
+        val calendarFragment=CalendarFragment()
+        val chatFragment=ChatFragment()
+        val optionsFragment=OptionsFragment()
+
+        setCurrentFragment(offersFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.offers_item->setCurrentFragment(offersFragment)
+                R.id.calendar_item->setCurrentFragment(calendarFragment)
+                R.id.chat_item->setCurrentFragment(chatFragment)
+                R.id.options_item->setCurrentFragment(optionsFragment)
+
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
 }
